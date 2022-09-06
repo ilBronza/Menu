f3235f654fb6d5c143e0ae3893e358fe2dfb5e3a
Davide Bronzini <davide_bronzini@hotmail.com>
2022-09-06 13:45:47 +0200

    Initial commit

---
+++ src/Menu.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace IlBronza\Menu;
+
+class Menu
+{
+    // Build wonderful things
+}
---
+++ src/MenuServiceProvider.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace IlBronza\Menu;
+
+use Illuminate\Support\ServiceProvider;
+
+class MenuServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'ilbronza');
+        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'ilbronza');
+        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        // $this->loadRoutesFrom(__DIR__.'/routes.php');
+
+        // Publishing is only necessary when using the CLI.
+        if ($this->app->runningInConsole()) {
+            $this->bootForConsole();
+        }
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/menu.php', 'menu');
+
+        // Register the service the package provides.
+        $this->app->singleton('menu', function ($app) {
+            return new Menu;
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['menu'];
+    }
+
+    /**
+     * Console-specific booting.
+     *
+     * @return void
+     */
+    protected function bootForConsole(): void
+    {
+        // Publishing the configuration file.
+        $this->publishes([
+            __DIR__.'/../config/menu.php' => config_path('menu.php'),
+        ], 'menu.config');
+
+        // Publishing the views.
+        /*$this->publishes([
+            __DIR__.'/../resources/views' => base_path('resources/views/vendor/ilbronza'),
+        ], 'menu.views');*/
+
+        // Publishing assets.
+        /*$this->publishes([
+            __DIR__.'/../resources/assets' => public_path('vendor/ilbronza'),
+        ], 'menu.views');*/
+
+        // Publishing the translation files.
+        /*$this->publishes([
+            __DIR__.'/../resources/lang' => resource_path('lang/vendor/ilbronza'),
+        ], 'menu.views');*/
+
+        // Registering package commands.
+        // $this->commands([]);
+    }
+}
