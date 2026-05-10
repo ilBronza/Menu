5a13872e288e5afa72c2436125111a2df3f0ab9c
ilBronza <davide_bronzini@hotmail.com>
2026-05-10 20:31:52 +0200

    Enhance horizontal menu: add dropdown mode configuration and improve navbar attributes handling

--- resources/views/uikit/menu/horizontal.blade.php
+++ resources/views/uikit/menu/horizontal.blade.php
@@ -2,20 +2,23 @@
 	$stickyAttrs = $menu->getSticky()
 		? 'sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky; end: + *; offset: ' . $menu->getStickyOffset()
 		: null;
+	/** Allineamento doc UIkit: mode su uk-navbar così tutti i .uk-navbar-dropdown prendono lo stesso comportamento. */
+	$navbarMode = trim((string) config('menu.buttons.dropdownMode', 'click'));
+	$navbarUkNavbarAttr = $navbarMode !== '' ? 'mode: ' . $navbarMode : null;
 @endphp
 @if($menu->getSticky())
 <div uk-sticky="{{ $stickyAttrs }}">
 @endif
 <div class="uk-navbar-container">
 	<div class="uk-container @if($menu->isFullWidth()) uk-container-expand @endif">
-		<nav class="uk-navbar {{ $menu->getWrapClass() }}" uk-navbar>
+		<nav class="uk-navbar {{ $menu->getWrapClass() }}" @if($navbarUkNavbarAttr) uk-navbar="{{ $navbarUkNavbarAttr }}" @else uk-navbar @endif>
 
 	@foreach($menu->getNavbarsByOrientation('horizontal') as $navbar)
 
 		@if($navbar->mustBreakRow())
 
 		</nav>
-		<nav class="uk-navbar-container {{ $menu->getWrapClass() }}" uk-navbar>
+		<nav class="uk-navbar-container {{ $menu->getWrapClass() }}" @if($navbarUkNavbarAttr) uk-navbar="{{ $navbarUkNavbarAttr }}" @else uk-navbar @endif>
 
 		@endif
 
